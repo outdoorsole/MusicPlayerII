773aa3215e5696705393d390374b2f1e50f35582
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-07 23:19:54 -0700

    Create Networking file to hold base URL, query dictionary, & construct query

---
+++ MusicPlayerII/Networking.swift
@@ -0,0 +1,35 @@
+//
+//  Networking.swift
+//  MusicPlayerII
+//
+//  Created by Maribel Montejano on 5/7/18.
+//  Copyright © 2018 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+// iTunes Search base URL
+let iTunesSearchBaseURL = URL(string: "http://itunes.apple.com/search")
+
+// URL extension: Use search queries to construct new URL (with key/value pairs as parameters)
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
+
+// MARK: - Helper method
+
+// creates new URL to query iTunes server with described search info
+func createURL(name: String) -> URL? {
+    // create query dictionary with selected music artist name
+    let iTunesQuery: [String: String] = [
+        "term": name,
+        "media": "music",
+        "lang": "us_en",
+        "limit": "100"
+    ]
+    return iTunesSearchBaseURL?.withQueries(iTunesQuery)
+}
