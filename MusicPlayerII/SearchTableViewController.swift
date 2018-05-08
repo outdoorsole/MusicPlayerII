a757e23e7d3c0352628beeb74ea09c58d563aa49
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-07 22:16:01 -0700

    Create SearchTableViewController & set initial configuration

---
+++ MusicPlayerII/SearchTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SearchTableViewController.swift
+//  MusicPlayerII
+//
+//  Created by Maribel Montejano on 5/7/18.
+//  Copyright © 2018 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
