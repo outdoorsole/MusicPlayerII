06980541367706a97fd72811acd6ada4421accc7
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-07 22:29:12 -0700

    Begin prepare for segue function

--- MusicPlayerII/SearchTableViewController.swift
+++ MusicPlayerII/SearchTableViewController.swift
@@ -36,14 +36,13 @@ class SearchTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("prepare for segue called")
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
