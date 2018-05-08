181bc11d92030db79a7d4191886345e51f1d4f05
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-07 22:35:02 -0700

    Create NowPlayingViewController; add IBOutlets for labels & view

---
+++ MusicPlayerII/NowPlayingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NowPlayingViewController.swift
+//  MusicPlayerII
+//
+//  Created by Maribel Montejano on 5/7/18.
+//  Copyright © 2018 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var albumArtView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
