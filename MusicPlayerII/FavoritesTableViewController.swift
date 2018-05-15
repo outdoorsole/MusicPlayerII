21ae39d360aa032fc13456737e93bad62c79c11b
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-05-14 20:30:18 -0700

    Delete favorite track from list and animate

--- MusicPlayerII/FavoritesTableViewController.swift
+++ MusicPlayerII/FavoritesTableViewController.swift
@@ -40,19 +40,16 @@ class FavoritesTableViewController: UITableViewController {
         return cell
     }
 
-
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            favorites.favoriteTracks.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+
 
 
     /*
